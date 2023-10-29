9354349a478135f14b3dbd39e7cba3a076317411
Nicky Bautista <nmbcold8@gmail.com>
2023-10-29 13:14:13 -0300

    Update fetch.js

--- assets/js/fetch.js
+++ assets/js/fetch.js
@@ -1,24 +1,44 @@
 document.addEventListener("DOMContentLoaded", function () {
-  // Realiza la consulta fetch y muestra el resultado en el div con id "resultado"
-  fetchDataAndDisplay();
+  // Obtiene la geolocalización y luego realiza la consulta fetch
+  getLocationAndFetchData();
 });
 
-function fetchDataAndDisplay() {
-  const url = 'https://api.weatherapi.com/v1/current.json?key=d97f0bedbd2344ff840153722232410&q=Buenos aires&aqi=no';
-  
+function getLocationAndFetchData() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(
+      function (position) {
+        // Obtiene la latitud y longitud del navegador
+        const latitude = position.coords.latitude;
+        const longitude = position.coords.longitude;
+
+        // Realiza la consulta fetch con la latitud y longitud
+        fetchDataAndDisplay(latitude, longitude);
+      },
+      function (error) {
+        console.error("Error getting location:", error);
+        // Si hay un error al obtener la geolocalización, puedes manejarlo aquí
+      }
+    );
+  } else {
+    console.error("Geolocation is not supported by this browser.");
+  }
+}
+
+function fetchDataAndDisplay(latitude, longitude) {
+  const url = `https://api.weatherapi.com/v1/current.json?key=d97f0bedbd2344ff840153722232410&q=${latitude},${longitude}&aqi=no`;
+
   fetch(url)
-  .then(response => response.json())
-  .then(data => {
+    .then((response) => response.json())
+    .then((data) => {
       // Llama a la función displayData para mostrar los resultados
       displayData(data);
 
       // Mostrar el icono del clima
       const imgElement = document.getElementById('climaIcono');
       imgElement.src = `http:${data.current.condition.icon}`;
-  })
-  .catch(error => console.error('Error:', error));
+    })
+    .catch((error) => console.error('Error:', error));
 }
-
 function displayData(data) {
   // Accede al div por su id
   const resultadoDiv = document.getElementById('resultado');
